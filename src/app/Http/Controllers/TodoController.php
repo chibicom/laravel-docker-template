7d5714ea288f1341a5acec968e1e9a1250d17e0d
chibicom <papipupepopopo68@gmail.com>
2024-05-26 23:26:28 +0900

    add: todo/index.blade.phpの実装と一覧画面の表示。routes/web.phpの編集。

---
+++ src/app/Http/Controllers/TodoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    // public function index()
+    // {
+    //     dd('Hello World!');
+    // }
+
+    public function index()
+    {
+        return view('todo.index');
+    }
+}
