6117b7d80ad85c3126df5253231ad12638b6d56c
chibicom <papipupepopopo68@gmail.com>
2024-05-27 10:23:38 +0900

    add: TodoModeelの作成とデータベースのマッピング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     // public function index()
@@ -13,6 +15,10 @@ class TodoController extends Controller
 
     public function index()
     {
+        $todo = new Todo();
+        $todoList = $todo->all();
+        // dd($todoList);
+        
         return view('todo.index');
     }
 }
