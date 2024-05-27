148046564afff69fc1883833307ce950c82587da
chibicom <papipupepopopo68@gmail.com>
2024-05-27 11:19:16 +0900

    feat:index.blade.php 追加ボタン設置, feat:routes/web.php ルート定義, add: create.blade.php 新規作成画面実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -21,4 +21,11 @@ class TodoController extends Controller
         
         return view('todo.index', ['todoList' => $todoList]);
     }
+
+    public function create()
+    {
+        // dd('新規作成画面のルート実行！');
+        // TODO: 第1引数を指定
+        return view('/todo/create'); // 追記
+    }
 }
